6a271abda3f559927f7320a446fa27819f57b221
Camila Contreras Barrera <camila1127@hotmail.ca>
2022-08-10 16:25:04 -0400

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning DB..."
+Restaurant.destroy_all
+
+puts "Seeding restaurants..."
+
+5.times do
+  rest = Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+  puts "Restaurant with id #{rest.id} was added"
+end
+
+puts 'Done!'
